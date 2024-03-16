5940838ea027bffcb441bb349302bc45b4778148
Antonio Morelli <4491145+victor00@users.noreply.github.com>
2024-03-16 20:58:44 -0300

    refactor: lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -17,7 +17,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
     { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.lang.javascript" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
